3b7ed6862429f114e62809a9b7c0eccd9718412e
anuragrazorpay <anurag.rastogi@razorpay.com>
2025-05-19 12:31:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const qrcode = require('qrcode-terminal');
 const QRCode = require('qrcode');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const sessionPath = process.env.WHATSAPP_SESSION || './whatsapp-session';
 
 app.use(express.json());
